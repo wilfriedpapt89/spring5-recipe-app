8c611afea07f858121ffd2e46decdf2c0ef178af
wilfriedpapt89 <wilfriedpapt@gmail.com>
2022-08-29 04:48:52 +0200

    continue with ingredient page

--- src/test/java/com/wilfried/spring5recipeapp/controllers/IngredientControllerTest.java
+++ src/test/java/com/wilfried/spring5recipeapp/controllers/IngredientControllerTest.java
@@ -4,6 +4,7 @@ import com.wilfried.spring5recipeapp.commands.IngredientCommand;
 import com.wilfried.spring5recipeapp.commands.RecipeCommand;
 import com.wilfried.spring5recipeapp.service.IngredientService;
 import com.wilfried.spring5recipeapp.service.RecipeService;
+import com.wilfried.spring5recipeapp.service.UnitOfMeasureService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
@@ -19,9 +20,13 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
+import java.util.HashSet;
+
 import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.anyLong;
 import static org.mockito.Mockito.*;
+import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
 @ExtendWith(MockitoExtension.class)
 @MockitoSettings(strictness = Strictness.LENIENT)
@@ -35,6 +40,9 @@ class IngredientControllerTest {
 
     @Mock
     RecipeService recipeService;
+
+    @Mock
+    UnitOfMeasureService unitOfMeasureService;
 
     MockMvc mockMvc;
 
@@ -54,8 +62,8 @@ class IngredientControllerTest {
         when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
 
         //when
-        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/ingredients"))
-                .andExpect(MockMvcResultMatchers.status().isOk())
+        mockMvc.perform(get("/recipe/1/ingredients"))
+                .andExpect(status().isOk())
                 .andExpect(MockMvcResultMatchers.view().name("recipe/ingredient/list"))
                 .andExpect(MockMvcResultMatchers.model().attributeExists("recipe"));
 
@@ -72,7 +80,26 @@ class IngredientControllerTest {
         //when
         when(ingredientService.findByRecipeIdAndIngredientId(anyLong(), anyLong())).thenReturn(ingredientCommand);
         //Then
-        mockMvc.perform(MockMvcRequestBuilders.get("/recipe/1/ingredient/1/show")).
-                andExpect(MockMvcResultMatchers.status().isOk()).andExpect(MockMvcResultMatchers.view().name("recipe/ingredient/show"));
+        mockMvc.perform(get("/recipe/1/ingredient/1/show")).
+                andExpect(status().isOk()).andExpect(MockMvcResultMatchers.view().name("recipe/ingredient/show"));
+    }
+
+    @Test
+    void testNewIngredientForm() throws Exception {
+
+        //Given
+        RecipeCommand recipeCommand = new RecipeCommand();
+        recipeCommand.setId(1L);
+
+        //Whene
+        when(recipeService.findCommandById(anyLong())).thenReturn(recipeCommand);
+        when(unitOfMeasureService.listAllUoms()).thenReturn(new HashSet<>());
+
+        //then
+        mockMvc.perform(get("/recipe/1/ingredient/new")).andExpect(status().isOk())
+                .andExpect(view().name("/recipe/ingredient/ingredientform"))
+                .andExpect(model().attributeExists("ingredient")).andExpect(model().attributeExists("uomList"));
+
+        verify(recipeService, times(1)).findCommandById(anyLong());
     }
 }
